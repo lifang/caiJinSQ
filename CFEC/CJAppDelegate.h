8bf19141494f364e8d26e6efadfe6e811850bf02
XuBingoo <251311016@qq.com>
2014-08-19 09:38:52 +0800

    Initial Commit

---
+++ CFEC/CJAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CJAppDelegate.h
+//  CFEC
+//
+//  Created by 徐宝桥 on 14-8-19.
+//  Copyright (c) 2014年 ___MyCompanyName___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CJAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CFEC/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CFEC
+//
+//  Created by 徐宝桥 on 14-8-19.
+//  Copyright (c) 2014年 ___MyCompanyName___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "CJAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([CJAppDelegate class]));
+    }
+}
